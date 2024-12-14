6b4d80764705d3dda4a94e6707b352f5e409fcf8
Denis-Mwanzia <denismwanzia56@gmail.com>
2024-12-14 18:30:41 +0300

    Covered basics concepts in javasript

---
+++ Beginner/01. Basics/basic.js
@@ -0,0 +1,25 @@
+// =============== CONSOLE ===============
+console.log('Hello from Denis');
+
+// console.log(console); // console object
+
+console.error('This is an error');
+console.warn('A Warning');
+console.table({
+  Name: 'Denis',
+  Age: 27,
+  Job: 'Fullstack Web Developer',
+  Country: ['Kenya'],
+});
+
+// Single Line Comment
+/*
+  Multiple 
+  Line
+  Comment
+ */
+/** Unordered Comment
+ * jkek
+ * kkjef
+ * kdvj
+ */
