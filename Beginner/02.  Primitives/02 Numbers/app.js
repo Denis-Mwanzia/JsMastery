d7439bf41d1704ecbc689d166db3edaee7fca089
Denis-Mwanzia <denismwanzia56@gmail.com>
2024-12-17 06:30:50 +0300

    Numbers - Primitive data type

---
+++ Beginner/02.  Primitives/02 Numbers/app.js
@@ -0,0 +1,10 @@
+// Represent both intergers and floating-point numbers
+
+let age = 25;
+let income = 45647;
+let tax = 0.175 * income;
+let afterTax = income - tax;
+
+console.log(
+  `Hello, I am ${age} years old and my monthly income is Ksh.${afterTax}`
+);
